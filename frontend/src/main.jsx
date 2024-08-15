ad94e8aec4c4298a745f642c0a2a10408cb1c862
kiran-jawale <kiran464jawale@gmail.com>
2024-08-15 22:35:25 +0530

    project structure set

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import Reacr from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+React.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+  
+ </React.StrictMode>
+  ,
+)
